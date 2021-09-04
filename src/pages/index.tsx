aa10644e04dbc28b889f7ca452f18ae5b83412de
JhonatanFrade <jhonatanfrade.pereira@gmail.com>
2021-09-04 12:49:00 -0300

    feat: Criado classe Tarefa

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,24 @@
+import Tarefa from "../model/Tarefa"
+
 export default function Home() {
+
+  const tarefa: Tarefa = new Tarefa(1, 'Exemplo de tarefa')
+  tarefa.concluir()
+
   return (
-    <div className="">
+    <div className={`
+      flex flex-col
+      justify-center
+      items-center
+      text-white
+      bg-gradient-to-tr
+      from-purple-500
+      to-yellow-600
+      h-screen
+    `}>
+      <span>{tarefa.id}</span>
+      <span>{tarefa.descricao}</span>
+      <span>{tarefa.concluida ? 'Concluída' : 'Ativa'}</span>
     </div>
   )
 }
