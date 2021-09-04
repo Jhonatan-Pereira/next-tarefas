c9f2c87ae2489508507ea467f100cadbd1088741
JhonatanFrade <jhonatanfrade.pereira@gmail.com>
2021-09-04 18:38:36 -0300

    feat: Criado componente ListaBotao

--- src/components/lista/Lista.tsx
+++ src/components/lista/Lista.tsx
@@ -1,4 +1,5 @@
 import ListaTarefas from "../../model/ListaTarefas";
+import ListaBotao from "./ListaBotao";
 import ListaItem from './ListaItem';
 
 interface ListaProps {
@@ -36,6 +37,13 @@ export default function Lista(props: ListaProps) {
         bg-white shadow-lg rounded-lg
       `}>
         {renderizarTarefas()}
+        <li className="p-5">
+          <ListaBotao 
+            selecionado={false} 
+            onClick={() => {}}>
+            Todas
+          </ListaBotao>
+        </li>
       </ul>
     </div>
   )
