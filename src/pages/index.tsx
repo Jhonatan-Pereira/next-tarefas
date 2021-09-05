7d0dc89e873f95019bc42432ce5692374fea307c
JhonatanFrade <jhonatanfrade.pereira@gmail.com>
2021-09-05 15:34:08 -0300

    feat: Criado formulário

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,18 @@
-import Tarefa from "../model/Tarefa";
 import tarefasIniciais from '../data/mock';
-import Selecao from '../components/lista/Selecao';
-import ListaItem from '../components/lista/ListaItem';
 import Lista from '../components/lista/Lista';
 import { useState } from "react";
 import Cabecalho from "../components/template/Cabecalho";
 import Conteudo from "../components/template/Conteudo";
+import Formulario from "../components/formulario/Formulario";
+import Tarefa from '../model/Tarefa';
 
 export default function Home() {
 
   const [tarefas, setTarefas] = useState(tarefasIniciais)
+
+  function novaTarefaCriada(novaTarefa: Tarefa) {
+    setTarefas(tarefas.adicionarTarefa(novaTarefa))
+  }
 
   return (
     <div className={`
@@ -18,7 +21,7 @@ export default function Home() {
       h-screen
     `}>
       <Cabecalho>
-        <h1>Teste</h1>
+        <Formulario novaTarefaCriada={novaTarefaCriada} />
       </Cabecalho>
       <Conteudo>
         <Lista tarefas={tarefas} mudou={(novasTarefas) => {
